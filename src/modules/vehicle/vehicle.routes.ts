b99dba747b869e97d63bd802561e4b274cac1982
abdullaalfahad <aafahad02@gmail.com>
2025-12-07 19:32:12 +0600

    feat: create vehicle only for admin

--- src/modules/vehicle/vehicle.routes.ts
+++ src/modules/vehicle/vehicle.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { auth } from '../../middlewares/auth';
 import { vehicleControllers } from './vehicle.controllers';
 
 const router: Router = Router();
 
-router.post('/', vehicleControllers.createVehicle);
+router.post('/', auth('admin'), vehicleControllers.createVehicle);
 
 export const vehicleRoutes = router;
